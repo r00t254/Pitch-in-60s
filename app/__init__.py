52ef40a122e5a27359cb3944bb5897c77168da45
r00t254 <root254.mary@gmail.com>
2022-05-10 09:50:52 +0300

    add changes

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
--- start.sh
+++
@@ -1,2 +0,0 @@
-
-python3.8 manage.py server
